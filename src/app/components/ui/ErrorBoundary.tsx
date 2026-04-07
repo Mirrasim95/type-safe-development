f005d7749b62fef1a30d0499269f5cfcbbe8f82e
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-04-07 16:32:09 +0400

    created class component for error boundary

---
+++ src/app/components/ui/ErrorBoundary.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Component, type ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {}
