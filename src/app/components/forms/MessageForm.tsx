4c4543d7f564f7c9e59e46ef0b26cd3bfad53d5a
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-04-13 17:38:38 +0400

    added map for messages

--- src/app/components/forms/MessageForm.tsx
+++ src/app/components/forms/MessageForm.tsx
@@ -3,8 +3,17 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MessageSchema, type MessageInput } from "@/schemas/message.schema";
+import { useState } from "react";
+import { ContentSafetyStatus } from "@/app/types/chat";
+
+type UIMessage = {
+  content: string;
+  safetyStatus: ContentSafetyStatus;
+};
 
 export function MessageForm() {
+  const [messages, setMessages] = useState<UIMessage[]>([]);
+
   const {
     register,
     handleSubmit,
@@ -13,12 +22,40 @@ export function MessageForm() {
     resolver: zodResolver(MessageSchema),
   });
 
-  function onSubmit(data: MessageInput) {
-    console.log(data);
+  async function onSubmit(data: MessageInput) {
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        content: result.data.content,
+        safetyStatus: result.data.safetyStatus,
+      },
+    ]);
   }
 
   return (
     <div className="flex flex-col max-w-xs">
+      {messages.map((item) => {
+        return (
+          <div key={index}>
+            <p>{item.content}</p>
+            <small>{item.safetyStatus}</small>
+          </div>
+        );
+      })}
       <input
         {...register("content")}
         type="text"
