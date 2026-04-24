61a4828671d723b97061a5d9a6f4e16e87a45f72
Rasim4ik111 <rasim95suleymanli@gmail.com>
2026-04-24 15:36:36 +0400

    added some styles

--- src/app/components/forms/MessageForm.tsx
+++ src/app/components/forms/MessageForm.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { MessageSchema, type MessageInput } from "@/schemas/message.schema";
 import { useState, useRef, useEffect } from "react";
 import { ContentSafetyStatus } from "@/app/types/chat";
+import Image from "next/image";
 
 type UIMessage = {
   content: string;
@@ -65,10 +66,22 @@ export function MessageForm() {
   }
 
   return (
-    <div className="flex flex-col h-screen max-w-2xl mx-auto border-x border-gray-200">
-      <h1 className="border-b w-full text-2xl p-3 flex justify-center border-gray-400">
-        Dualingo Chat Room
-      </h1>
+    <div className="flex flex-col h-screen max-w-2xl mx-auto border-x border-gray-200 bg-[#FCF9EC] shadow-2xl">
+      <div className="flex bg-[#E0F3DE] p-4 justify-center shadow-lg rounded-bl-2xl rounded-br-2xl">
+        <Image
+          src={"/duolingo-1.svg"}
+          width={60}
+          height={60}
+          alt="dualingo-svg"
+        />
+        <Image
+          src={"/duolingo-2019.svg"}
+          width={150}
+          height={150}
+          alt="dualingo-svg"
+          className="pl-5"
+        />
+      </div>
       <div className="flex-1 overflow-y-auto p-4 flex flex-col gap-3 ml-1 ">
         {messages.map((item, index) => (
           <div
@@ -85,12 +98,12 @@ export function MessageForm() {
       </div>
 
       {/* Инпут — фиксирован снизу */}
-      <div className="p-4 border-t border-gray-700 flex gap-2">
+      <div className="p-4 flex gap-2 bg-[#264F39] ">
         <input
           {...register("content")}
           type="text"
           placeholder="Type a message..."
-          className="flex-1 border rounded-2xl border-gray-400 p-2"
+          className="flex-1 border rounded-2xl border-gray-400 p-2 bg-amber-50"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               handleSubmit(onSubmit)();
@@ -98,7 +111,7 @@ export function MessageForm() {
           }}
         />
         <button
-          className="border rounded-2xl px-4 py-2 active:bg-gray-600"
+          className="border border-green-600 rounded-2xl px-4 py-2 active:bg-gray-600 bg-[#64C601] text-white"
           onClick={handleSubmit(onSubmit)}
         >
           Send
